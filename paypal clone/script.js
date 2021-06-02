579a265b96948f4cbd0951b3fc36c821fd42af30
ShrutiKulkarni156 <72867925+ShrutiKulkarni156@users.noreply.github.com>
2021-06-02 18:03:39 +0530

    Add files via upload

---
+++ paypal clone/script.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.logo').forEach(logo=>{
+    logo.addEventListener('click',()=>{
+        document.querySelector('.front-page').style.display='block'
+        document.querySelector('.login-page').style.display='none'
+        document.querySelector('.signup-page').style.display='none'
+    })
+})
+document.querySelectorAll('.login').forEach(loginBtn=>{
+    loginBtn.addEventListener('click',()=>{
+        document.querySelector('.front-page').style.display='none'
+        document.querySelector('.login-page').style.display='block'
+        document.querySelector('.signup-page').style.display='none'
+    })
+})
+document.querySelectorAll('.signup').forEach(signupBtn=>{
+    signupBtn.addEventListener('click',()=>{
+        document.querySelector('.front-page').style.display='none'
+        document.querySelector('.login-page').style.display='none'
+        document.querySelector('.signup-page').style.display='block'
+    })
+})
+
+const dropdownItems=document.querySelectorAll('.dropdown-hover')
+dropdownItems.forEach(dropdownItems=>{
+   
+        dropdownItems.addEventListener('mouseover',()=>{
+            dropdownItems.lastElementChild.style.cssText=
+            'opacity:1;visibility:visible'
+            document.querySelector('.navbar-wrapper').style.
+            background='linear-gradient(to right,#066399,#2f8fdf,#066399)'
+        })
+   
+    dropdownItems.addEventListener('mouseout',()=>{
+        dropdownItems.lastElementChild.style.cssText=
+        'opacity:0;visibility:hidden'
+        document.querySelector('.navbar-wrapper').style.
+        background='none'
+    })
+})
